7a3fc1e86055ed49756aa44bf6b50462c8f6d1b2
Kuldip-Mitra <kuldipmitra1@gmail.com>
2024-11-10 15:15:01 +0000

    1st Module changed

--- .devcontainer/basics_js/test.js
+++ .devcontainer/basics_js/test.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+const accountId = 144553
+let accountEmail = "kuldipmitra@1gmail.com"
+var accountPassword = "12345"
+accountCity = "Kolkata"
+
+//accountId = 2
+accountEmail = "hc@gmail.com"
+accountPassword = "54321"
+accountCity = "Bengaluru"
+let accountState;
+
+console.log(accountId);
+
+
+//Prefer not to use var
+//because of issue in block scope and functional scope
+
+
+console.table([accountId, accountPassword, accountEmail, accountCity, accountState])
